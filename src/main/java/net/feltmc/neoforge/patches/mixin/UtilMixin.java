3413c6e3745da75281501760f589ebb7309cd258
Nolij <xdmatthewbx@mixhosting.xyz>
2023-09-22 17:29:26 -0400

    more patches go brrrr

---
+++ src/main/java/net/feltmc/neoforge/patches/mixin/UtilMixin.java
@@ -0,0 +1,17 @@
+package net.feltmc.neoforge.patches.mixin;
+
+import net.minecraft.Util;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(Util.class)
+public class UtilMixin {
+	
+	@Redirect(method = "doFetchChoiceType", at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;error(Ljava/lang/String;Ljava/lang/Object;)V"))
+	private static void doFetchChoiceType$error(Logger instance, String s, Object o) {
+		instance.debug(s, o);
+	}
+	
+}
